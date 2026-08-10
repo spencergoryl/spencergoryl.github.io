1f34add4718fd857a00686c10e9f1ac680e8d965
spencergoryl <126640274+spencergoryl@users.noreply.github.com>
2026-08-10 00:39:50 -0700

    Refactor particle animation and canvas setup

--- script.js
+++ script.js
@@ -2,139 +2,129 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 document.fonts.ready.then(() => {
-    
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
 
-const particles = [];
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
-const particleCount = 1200;
+    const particles = [];
+    const particleCount = 1200;
 
+    // --------------------------------
+    // Create an invisible version of ENGRAM
+    // --------------------------------
 
-// --------------------------------
-// Create an invisible version of ENGRAM
-// --------------------------------
+    const textCanvas = document.createElement("canvas");
+    const textCtx = textCanvas.getContext("2d");
 
-const textCanvas = document.createElement("canvas");
-const textCtx = textCanvas.getContext("2d");
+    textCanvas.width = canvas.width;
+    textCanvas.height = canvas.height;
 
-textCanvas.width = canvas.width;
-textCanvas.height = canvas.height;
+    textCtx.fillStyle = "white";
 
-textCtx.fillStyle = "white";
+    const fontSize = Math.min(canvas.width * 0.18, 180);
 
-const fontSize = Math.min(canvas.width * 0.18, 180);
+    textCtx.font = `700 ${fontSize}px "Averia Serif Libre"`;
+    textCtx.textAlign = "center";
+    textCtx.textBaseline = "middle";
 
-textCtx.font = `700 ${100}px "Averia Serif Libre"`;
+    textCtx.fillText(
+        "ENGRAM",
+        canvas.width / 2,
+        canvas.height / 2
+    );
 
-textCtx.textAlign = "center";
-textCtx.textBaseline = "middle";
+    // --------------------------------
+    // Find the pixels that make up the word
+    // --------------------------------
 
-textCtx.fillText(
-    "ENGRAM",
-    canvas.width / 2,
-    canvas.height / 2
-);
-
-
-// --------------------------------
-// Find the pixels that make up the word
-// --------------------------------
-
-const imageData = textCtx.getImageData(
-    0,
-    0,
-    canvas.width,
-    canvas.height
-);
-
-const pixels = [];
-
-for (let y = 0; y < canvas.height; y += 5) {
-
-    for (let x = 0; x < canvas.width; x += 5) {
-
-        const index = (y * canvas.width + x) * 4;
-
-        const alpha = imageData.data[index + 3];
-
-        if (alpha > 128) {
-
-            pixels.push({
-                x: x,
-                y: y
-            });
-
-        }
-
-    }
-
-}
-
-
-// --------------------------------
-// Create particles
-// --------------------------------
-
-for (let i = 0; i < particleCount; i++) {
-
-    const target = pixels[i % pixels.length];
-
-    particles.push({
-
-        x: Math.random() * canvas.width,
-        y: Math.random() * canvas.height,
-
-        targetX: target.x,
-        targetY: target.y,
-
-        size: Math.random() * 2 + 0.5
-
-    });
-
-}
-
-
-// --------------------------------
-// Animate
-// --------------------------------
-
-function animate() {
-
-    ctx.clearRect(
+    const imageData = textCtx.getImageData(
         0,
         0,
         canvas.width,
         canvas.height
     );
 
-    ctx.fillStyle = "white";
+    const pixels = [];
 
-    particles.forEach(particle => {
+    for (let y = 0; y < canvas.height; y += 5) {
 
-        particle.x +=
-            (particle.targetX - particle.x) * 0.01;
+        for (let x = 0; x < canvas.width; x += 5) {
 
-        particle.y +=
-            (particle.targetY - particle.y) * 0.01;
+            const index = (y * canvas.width + x) * 4;
+            const alpha = imageData.data[index + 3];
 
+            if (alpha > 128) {
 
-        ctx.beginPath();
+                pixels.push({
+                    x: x,
+                    y: y
+                });
 
-        ctx.arc(
-            particle.x,
-            particle.y,
-            particle.size,
+            }
+        }
+    }
+
+    // --------------------------------
+    // Create particles
+    // --------------------------------
+
+    for (let i = 0; i < particleCount; i++) {
+
+        const target = pixels[i % pixels.length];
+
+        particles.push({
+
+            x: Math.random() * canvas.width,
+            y: Math.random() * canvas.height,
+
+            targetX: target.x,
+            targetY: target.y,
+
+            size: Math.random() * 2 + 0.5
+
+        });
+    }
+
+    // --------------------------------
+    // Animate
+    // --------------------------------
+
+    function animate() {
+
+        ctx.clearRect(
             0,
-            Math.PI * 2
+            0,
+            canvas.width,
+            canvas.height
         );
 
-        ctx.fill();
+        ctx.fillStyle = "white";
 
-    });
+        particles.forEach(particle => {
 
-    requestAnimationFrame(animate);
+            particle.x +=
+                (particle.targetX - particle.x) * 0.01;
 
-}
+            particle.y +=
+                (particle.targetY - particle.y) * 0.01;
 
-animate();
+            ctx.beginPath();
+
+            ctx.arc(
+                particle.x,
+                particle.y,
+                particle.size,
+                0,
+                Math.PI * 2
+            );
+
+            ctx.fill();
+
+        });
+
+        requestAnimationFrame(animate);
+    }
+
+    animate();
+
+});
