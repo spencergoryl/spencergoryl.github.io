103e0279bf102ec5bae0443efdffdac98a00588e
spencergoryl <126640274+spencergoryl@users.noreply.github.com>
2026-08-10 00:29:48 -0700

    Initialize canvas size on font load
    Set canvas dimensions to match window size after fonts are ready.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+document.fonts.ready.then(() => {
+    
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
