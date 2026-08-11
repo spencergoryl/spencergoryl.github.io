21c9249f6fac82fd84f08f9c1ba8c199240f233a
spencergoryl <126640274+spencergoryl@users.noreply.github.com>
2026-08-10 23:58:23 -0700

    Adjust font appearance and scaling parameters

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ document.fonts.ready.then(() => {
     // --------------------------------
 
     const MELT_SPEED = 0.012;
-    const FONT_APPEAR_POINT = 0.5;
+    const FONT_APPEAR_POINT = 0.6;
 
-    const FONT_START_SCALE = 1.06;
-    const FONT_SETTLE_SPEED = 0.08;
+    const FONT_START_SCALE = 1.03;
+    const FONT_SETTLE_SPEED = 0.1;
 
     const MAX_FONT_SIZE = 100;
 
