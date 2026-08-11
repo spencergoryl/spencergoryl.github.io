2b5951c2d9211b112e4dbd57d1f4cac010b55ada
spencergoryl <126640274+spencergoryl@users.noreply.github.com>
2026-08-11 00:07:54 -0700

    Adjust FONT_START_SCALE from 1.04 to 1.03

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ document.fonts.ready.then(() => {
     const MELT_SPEED = 0.012;
     const FONT_APPEAR_POINT = 0.7;
 
-    const FONT_START_SCALE = 1.04;
+    const FONT_START_SCALE = 1.03;
     const FONT_SETTLE_SPEED = 0.1;
 
     const MAX_FONT_SIZE = 100;
