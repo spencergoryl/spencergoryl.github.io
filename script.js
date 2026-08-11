458390060d4fb48785768a511ccd8d35cc6b7fae
spencergoryl <126640274+spencergoryl@users.noreply.github.com>
2026-08-11 00:55:08 -0700

    Adjust font scale and particle contract speed constants

--- script.js
+++ script.js
@@ -16,10 +16,10 @@ document.fonts.ready.then(() => {
     const MELT_SPEED = 0.012;
     const FONT_APPEAR_POINT = 0.7;
 
-    const FONT_START_SCALE = 1.015;
+    const FONT_START_SCALE = 1.01;
     const FONT_SETTLE_SPEED = 0.002;
 
-    const PARTICLE_CONTRACT_SPEED = 0.08;
+    const PARTICLE_CONTRACT_SPEED = 0.1;
 
     const MAX_FONT_SIZE = 100;
 
