d0f5ca70501d280218c75c7eb10983e4f9c891f8
spencergoryl <126640274+spencergoryl@users.noreply.github.com>
2026-08-10 23:52:20 -0700

    Adjust font appearance settings
    Updated font appearance settings for animation.

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ document.fonts.ready.then(() => {
     // --------------------------------
 
     const MELT_SPEED = 0.012;
-    const FONT_APPEAR_POINT = 0.47;
+    const FONT_APPEAR_POINT = 0.87;
 
-    const FONT_START_SCALE = 1.04;
-    const FONT_SETTLE_SPEED = 0.18;
+    const FONT_START_SCALE = 1.06;
+    const FONT_SETTLE_SPEED = 0.12;
 
     const MAX_FONT_SIZE = 100;
 
